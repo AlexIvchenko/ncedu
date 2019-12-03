e6b557c623c698e0bb735aa9ead20aed2f364235
Alexey <me@krvd.ru>
2019-12-03 03:01:41 +0300

    NC_EDU-57: Test version

--- src/main/java/com/ncedu/nc_edu/controllers/RootController.java
+++ src/main/java/com/ncedu/nc_edu/controllers/RootController.java
@@ -1,17 +1,17 @@
 package com.ncedu.nc_edu.controllers;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
 import com.ncedu.nc_edu.security.SecurityAccessResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class RootController {
@@ -27,8 +27,8 @@ public class RootController {
         responseMap.put("authenticated", securityAccessResolver.getUser() != null);
         EntityModel response = new EntityModel<>(responseMap);
         response.add(linkTo(methodOn(RootController.class).root()).withSelfRel().withType("GET"));
-        response.add(linkTo(methodOn(RecipeController.class).getAll(null, null)).withRel("recipes").withType("GET"));
-        response.add(linkTo(methodOn(RecipeController.class).search(null, null, null)).withRel("recipe search").withType("GET"));
+        response.add(linkTo(methodOn(RecipeController.class).getAll(null, null, null)).withRel("recipes").withType("GET"));
+        response.add(linkTo(methodOn(RecipeController.class).search(null, null, null, null)).withRel("recipe search").withType("GET"));
         if (securityAccessResolver.getUser() == null) {
             response.add(linkTo(methodOn(UserController.class).add(null, null)).withRel("register").withType("GET"));
         } else {
